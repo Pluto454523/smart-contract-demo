1fa6ddacd122e9cd9f2045844afb5ca851660a24
Pluto454523 <nawin3399@gmail.com>
2024-10-06 20:06:16 +0700

    update

---
+++ contracts/MyContract.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26; 
+// ประกาศ pragma เพื่อใช้เลือกภาษาในการ compile
+// โดยในที่นี้เราเลือกใช้ solidity เวอร์ชั่น 0.8.26 
+
+contract MyContract { // ประกาศ Class MyContract โดยใช้ syntax contract
+    string _name;
+    uint _balance;
+
+    // การเรียกใช้งาน constructor เพื่อเป็นการเปิดบัญชีโดยใช้ ชื่อ กับ จำนวนเงินที่ต้องการเปิดบัญชี
+    // จะมีการเสียค่า Gas เป็นค่าธรรมเนียมในการทำงาน
+    constructor(string memory name, uint balance) {
+        require(balance >= 500, "balance must be greater and equal 500");
+        _name = name;
+        _balance = balance;
+    }
+
+    // getBalance เพื่อดูยอดเงิน ไม่เสียค่า Gas เพราะเป็นฟังชั่นสำหรับดูอย่างเดียว
+    function getBalance() public view returns(uint balance) {
+        return _balance;
+    }
+
+    // deposite ฟังชั่นในการฝากเงินเข้าบัญชี
+    // การเรียกใช้ deposite จะมีการเก็บค่า Gas เพราะมีการทำ transaction ในระบบ smart contract
+    function deposite(uint amount) public {
+        _balance += amount;
+    }
+
+}
